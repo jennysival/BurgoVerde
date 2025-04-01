11c4fda90012e53fc3229532c5e3f5394d94e4ce
jennysival <jeniffersival@gmail.com>
2025-04-01 15:10:03 -0300

    feat(Onboarding): setup onboarding navigation

--- app/src/main/java/com/jennysival/burgoverde/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/jennysival/burgoverde/ui/onboarding/OnboardingFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import com.jennysival.burgoverde.R
 import com.jennysival.burgoverde.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : Fragment() {
@@ -21,6 +23,21 @@ class OnboardingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        userRegisterClick()
+        userLoginClick()
+    }
 
+    private fun userRegisterClick() {
+        binding.registerBtn.setOnClickListener {
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_onboardingFragment_to_userRegisterFragment)
+        }
+    }
+
+    private fun userLoginClick() {
+        binding.loginBtn.setOnClickListener {
+            NavHostFragment.findNavController(this)
+                .navigate(R.id.action_onboardingFragment_to_loginFragment)
+        }
     }
 }
