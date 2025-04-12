13e95b3c130531cc6bc6f6c254bdc8a65e5c3606
jennysival <jeniffersival@gmail.com>
2025-04-12 16:27:56 -0300

    feat(Plant): create plant repository

---
+++ app/src/main/java/com/jennysival/burgoverde/repository/plant/PlantRepository.kt
@@ -0,0 +1,11 @@
+package com.jennysival.burgoverde.repository.plant
+
+import android.net.Uri
+import com.jennysival.burgoverde.data.PlantModel
+
+interface PlantRepository {
+    suspend fun savePlant(plant: PlantModel, imageUri: Uri): Result<Unit>
+    suspend fun syncPlantsFromFirestore(): Result<Unit>
+    suspend fun getAllPlants(): List<PlantModel>
+    suspend fun deletePlant(plant: PlantModel)
+}
